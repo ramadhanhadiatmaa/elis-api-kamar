3fd048286556925150255de795c3310f57dd9444
ayomenjadi <ayomenjadi@gmail.com>
2024-11-12 19:59:41 +0000

    ci(dockerfile): Add dockerfile to CI/CD

--- models/kamar.go
+++ models/kamar.go
@@ -3,6 +3,7 @@ package models
 type Kamar struct {
 	Id      string `gorm:"type:varchar(25); primaryKey" json:"id"`
 	Bangsal string `gorm:"type:varchar(150)" json:"bangsal"`
+	Jenis   string `gorm:"type:varchar(50)" json:"jenis"`
 	Total   string `gorm:"type:varchar(250)" json:"total"`
 	Isi     string `gorm:"type:varchar(50)" json:"isi"`
 }
